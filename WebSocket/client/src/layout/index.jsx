137a7e9f911b0ab8162eb5502259d73b4196116e
EsunR <641411169@qq.com>
2020-04-09 20:59:53 +0800

    commit

---
+++ WebSocket/client/src/layout/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Layout extends React.Component {
+  render() {
+    return (
+      <div id="layout">
+        <header>头部</header>
+        <section>主要部分</section>
+        <footer>足部</footer>
+      </div>
+    );
+  }
+}
+
+export default Layout;
