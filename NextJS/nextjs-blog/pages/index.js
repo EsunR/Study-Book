4ab9052a55dcf42e4346e747cf52f36dd046a405
EsunR <641411169@qq.com>
2021-03-22 14:33:08 +0800

    add: nextjs

---
+++ NextJS/nextjs-blog/pages/index.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>I Love ReactJS</p>
+        <Link href="/post/first-post">
+          <a>My First post</a>
+        </Link>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://www.nextjs.cn/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
---
+++ NextJS/nextjs-blog/pages/post/first-post.js
@@ -0,0 +1,13 @@
+import Head from "next/head";
+import Layout from "../../components/layout";
+
+export default function FirstPost() {
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+    </Layout>
+  );
+}
